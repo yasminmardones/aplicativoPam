7b1657b6cb2f8953aa108380a2578efd6a37fc64
pcetec <pc@etec.com>
2026-09-16 20:51:41 -0300

    manipulando elementos no kotlin

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +11,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.aulaid)
+        val botao = findViewById<Button>(R.id.btnProxima)
+        val texto = findViewById<EditText>(R.id.editValor)
 
-        setContentView(R.layout.exercicio_01)
+
+        botao.text = "Oi testando"
+
+        botao.setOnClickListener {
+            botao.text = texto.text
+        }
+
+
+
+
 
 
         }
